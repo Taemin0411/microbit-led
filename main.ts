4cda77288d7a9d4b27ac6fb50534d3277044e57f
Taemin Kim <100333592+Taemin0411@users.noreply.github.com>
2022-10-14 16:14:29 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendValue("do", 0)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendValue("do", 1)
+})
+radio.onReceivedValue(function (name, value) {
+    if (value == 0) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    } else if (value == 1) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    }
+})
+radio.setGroup(408)
+basic.showLeds(`
+    # . # . #
+    # . # . .
+    # # # . #
+    # . # . #
+    # . # . #
+    `)
+basic.pause(500)
